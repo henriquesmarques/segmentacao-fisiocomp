e7312253da79a4c324c7da53963b2d7c8d521512
Henrique Silveira Marques <henrique.silveira@estudante.ufjf.br>
2025-06-10 08:35:25 -0300

    Reescrevendo arquivo .MAT

--- find_contours.py
+++ find_contours.py
@@ -1,103 +1,177 @@
 import os
-import numpy as np # type: ignore
-import nibabel as nib # type: ignore
-from skimage.measure import find_contours # type: ignore
+import numpy as np
 import matplotlib.pyplot as plt # type: ignore
-import cv2 as cv # type: ignore
-from scipy.io import savemat # type: ignore
+import scipy.io as sio # type: ignore
+from nibabel import load # type: ignore
+from cv2 import dilate # type: ignore
+from skimage.measure import find_contours # type: ignore
 
 data_dir = os.getcwd()
+data_list = sorted(os.listdir(f'{data_dir}/input'))
 section = ['ED', 'ES']
-os.makedirs(os.path.join(f'{data_dir}/output', 'contours-txt'), exist_ok=True)
-os.makedirs(os.path.join(f'{data_dir}/output', 'contours-png'), exist_ok=True)
 
-for fr in section:
-    # Lendo a imagem
-    image_name = '{0}/{1}.nii.gz'.format(f'{data_dir}/input', f'seg_sa_{fr}')
-    if not os.path.exists(image_name):
-        print('  Directory {0} does not contain an image with file '
-                'name {1}. Skip.'.format(data_dir, os.path.basename(image_name)))
-        continue
-    print('  Reading {} ...'.format(image_name))
-    # Convertendo imagem em um array numpy
-    nim = nib.load(image_name)
-    image = nim.get_fdata()
-    X, Y, Z = image.shape
+for data in data_list:
+    os.makedirs(os.path.join(f'{data_dir}/output/{data}', 'contours-txt'), exist_ok=True)
+    os.makedirs(os.path.join(f'{data_dir}/output/{data}', 'contours-png'), exist_ok=True)
+    try:
+        mat = sio.loadmat(f'{data_dir}/input/paciente_cine_2/Patient_2.mat')
+        print('    Arquivo .MAT encontrado.')
+    except FileNotFoundError:
+        print('    Erro: Arquivo .MAT não encontrado.')
 
-    for frame in range(Z):
-        # Convertendo a imagem para 2D
-        slice_2d = image[:, :, frame]
-        # Extraindo quantidade de cores encontradas na segmentação
-        unique_colors = np.unique(slice_2d)
+
+    for fr in section:
+        # Lendo a imagem
+        image_name = '{0}/{1}.nii.gz'.format(f'{data_dir}/input/{data}', f'seg_sa_{fr}')
+        if not os.path.exists(image_name):
+            print('   Directory {0} does not contain an image with file '
+                    'name {1}. Skip.'.format(data_dir, os.path.basename(image_name)))
+            continue
+        print('  Reading {} ...'.format(image_name))
+
+        nim = load(image_name)
+        image = nim.get_fdata()
+        X, Y, Z = image.shape
+
         # Dicionário para armazenar os contornos
         contours_dict = {}
-        # Iterando sobre cada cor única para extrair e salvar seus contornos
-        fig, ax = plt.subplots(figsize=(Y / 25, X / 25))
+        
+        for frame in range(Z):
+            # Convertendo a imagem para 2D
+            slice_2d = image[:, :, frame]
+            # Definindo tamanho da plotagem
+            fig, ax = plt.subplots(figsize=(Y / 25, X / 25))
 
-        # Ventrículo esquerdo
-        mask = (slice_2d == 1)
-        # Encontrando os contornos na máscara
-        contours = find_contours(mask, level=0.5)
-        # Salvando os contornos de cada máscara no dicionário
-        contours_dict[f'{fr}_{frame}_VE'] = [contour.tolist() for contour in contours]
-        # Plotando os contornos
-        for contour in contours:
-            ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='red')
+            # Ventrículo esquerdo (EndoX)
+            mask = (slice_2d == 1)
+            # Encontrando os contornos na máscara
+            contours = find_contours(mask, level=0.5)
+            # print(contours)
+            # Salvando os contornos de cada máscara no dicionário
+            contours_dict[f'{fr}_{frame}_VE'] = [contour.tolist() for contour in contours]
+            # Plotando os contornos
+            for contour in contours:
+                ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='red')
+                
+            # Ventrículo direito
+            mask = (slice_2d == 3)
+            # Encontrando os contornos na máscara
+            contours = find_contours(mask, level=0.5)
+            # Salvando os contornos de cada máscara no dicionário
+            contours_dict[f'{fr}_{frame}_VD'] = [contour.tolist() for contour in contours]
+            # Plotando os contornos
+            for contour in contours:
+                ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='red')
 
-        # Ventrículo direito
-        mask = (slice_2d == 3)
-        # Encontrando os contornos na máscara
-        contours = find_contours(mask, level=0.5)
-        # Salvando os contornos de cada máscara no dicionário
-        contours_dict[f'{fr}_{frame}_VD'] = [contour.tolist() for contour in contours]
-        # Plotando os contornos
-        for contour in contours:
-            ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='red')
+            # Adicionando padding de 3mm
+            # Obtendo o espaçamento dos pixels (x, y)
+            pixel_spacing = nim.header['pixdim'][1:3]  
+            # Convertendo 3mm para pixels
+            kernel_size = max(3, int(np.ceil(3 / np.mean(pixel_spacing))))
+            # Cria uma matriz quadrada de tamanho kernel_size x kernel_size
+            kernel = np.ones((kernel_size, kernel_size), np.uint8)
+            # Técnica de dilatação
+            mask = dilate(mask.astype(np.uint8), kernel, iterations=1)
+            # Subtraindo interseção entre a máscara 2
+            mask = mask & ~(slice_2d == 2)
+            # Adicionando padding na imagem original
+            slice_2d += mask
 
-        # Adicionando padding de 3mm
-        # Obtendo o espaçamento dos pixels (x, y)
-        pixel_spacing = nim.header['pixdim'][1:3]  
-        # Convertendo 3mm para pixels
-        kernel_size = max(3, int(np.ceil(3 / np.mean(pixel_spacing))))
-        # Cria uma matriz quadrada de tamanho kernel_size x kernel_size
-        kernel = np.ones((kernel_size, kernel_size), np.uint8)
-        # Técnica de dilatação
-        mask = cv.dilate(mask.astype(np.uint8), kernel, iterations=1)
-        # Subtraindo interseção entre a máscara 2
-        mask = mask & ~(slice_2d == 2)
-        # Adicionando padding na imagem original
-        slice_2d += mask
+            # Extraindo contornos da segmentação completa com adição do padding
+            contours = find_contours(slice_2d, level=0.1)
+            # Salvando os contornos no dicionário
+            contours_dict[f'{fr}_{frame}_EP'] = [contour.tolist() for contour in contours]
+            # Plotando os contornos
+            for contour in contours:
+                ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='blue')
 
-        # Extraindo contornos da segmentação completa com adição do padding
-        contours = find_contours(slice_2d, level=0.1)
-        # Salvando os contornos no dicionário
-        contours_dict[f'{fr}_{frame}_EP'] = [contour.tolist() for contour in contours]
-        # Plotando os contornos
-        for contour in contours:
-            ax.plot(contour[:, 1], contour[:, 0], linewidth=0.5, color='blue')
-
-        # Salvando os contornos em uma imagem .jpg
-        contour_image_path = os.path.join(f'{data_dir}/output/contours-png', f'{fr}_{frame}.png')
-        # ax.imshow(slice_2d)
-        plt.axis('off')
-        plt.savefig(contour_image_path, pad_inches=0)
-        plt.close(fig)
-
+            # Salvando os contornos em uma imagem .jpg
+            contour_image_path = os.path.join(f'{data_dir}/output/{data}/contours-png', f'{fr}_{frame}.png')
+            # ax.imshow(slice_2d)
+            plt.axis('off')
+            plt.savefig(contour_image_path, pad_inches=0)
+            plt.close(fig)
+            
         # Salvando os contornos em arquivos .txt separados para cada key
         for key, contours in contours_dict.items():
-            key_file_path = os.path.join(f'{data_dir}/output/contours-txt', f'{key}.txt')
+            key_file_path = os.path.join(f'{data_dir}/output/{data}/contours-txt', f'{key}.txt')
             with open(key_file_path, 'w') as txt_file:
-                # txt_file.write(f'{key}:\n')
                 for contour in contours:
-                    for point in contour:
-                        txt_file.write(f'{point[0]:.6f} {point[1]:.6f}\n')
-                    # print(f'    Contornos salvos em {key_file_path}')
+                    for i, point in enumerate(contour):
+                        if i % 6 == 0:
+                            txt_file.write(f'{point[0]:.6f} {point[1]:.6f} 0\n')
 
-        print(f'    Fatia {fr} {frame} salva.')
+        # Salvando os contornos no arquivo .mat de entrada
+        endox = np.zeros(shape = (80,1,Z))
+        endoy = np.zeros(shape = (80,1,Z))
+        rvendox = np.zeros(shape = (80,1,Z))
+        rvendoy = np.zeros(shape = (80,1,Z))
+        rvepix = np.zeros(shape = (80,1,Z))
+        rvepiy = np.zeros(shape = (80,1,Z))
+    
+        for key, contours in contours_dict.items():
+            for contour in contours:
+                # print(f'        {key}')
+                if (f'{fr}_{frame}_VE' == key):
+                    for i, point in enumerate(contour):
+                        if i < 80:  # Limite de 80 pontos
+                            endoy[i, 0, frame] = point[0]
+                            endox[i, 0, frame] = point[1]
+                elif (f'{fr}_{frame}_VD' == key):
+                    for i, point in enumerate(contour):
+                        if i < 80:  # Limite de 80 pontos
+                            rvendoy[i, 0, frame] = point[0]
+                            rvendox[i, 0, frame] = point[1]
+                elif (f'{fr}_{frame}_EP' == key):
+                    for i, point in enumerate(contour):
+                        if i < 80:  # Limite de 80 pontos
+                            rvepiy[i, 0, frame] = point[0]
+                            rvepix[i, 0, frame] = point[1]
+            
+            
+        # print(contours_dict)
+        """ for key, contours in contours_dict.items():
+            for contour in contours:
+                # print(f'        {key}')
+                print(f'{fr}_{frame}_VE')
+                k = l = m = 0
+                for point in contour:
+                    if (f'{fr}_{frame}_VE' == key):
+                        # print(f'        {key}')
+                        endoy[k,0,frame] = point[0]
+                        endox[k,0,frame] = point[1]
+                        k = k + 1
+                    if (f'{fr}_{frame}_VD' == key):
+                        # print(f'        {key}')
+                        rvendoy[l,0,frame] = point[0]
+                        rvendox[l,0,frame] = point[1]
+                        l = l + 1
+                    if (f'{fr}_{frame}_EP' == key):
+                        # print(f'        {key}')
+                        rvepiy[m,0,frame] = point[0]
+                        rvepix[m,0,frame] = point[1]
+                        m = m + 1 """
 
-    """ # Salvando os contornos em um arquivo .mat
-    mat_file_path = os.path.join(f'{data_dir}/contours/mat', f'contours_{fr}.mat')
-    savemat(mat_file_path, contours_dict)
-    print(f'  Contornos salvos em {mat_file_path}') """
+        # Reescrevendo arquivo .MAT
+        if 'setstruct' in mat: 
+            print('    Dimensions EndoX: ', mat['setstruct']['EndoX'][0][0].ndim)
+            print('    Shape EndoX: ', mat['setstruct']['EndoX'][0][0].shape)
+            print('    Size EndoX: ', mat['setstruct']['EndoX'][0][0].size)
+            
+            mat['setstruct']['EndoX'][0][0] = endox
+            mat['setstruct']['EndoY'][0][0] = endoy
+            mat['setstruct']['RVEndoX'][0][0] = rvendox
+            mat['setstruct']['RVEndoY'][0][0] = rvendoy
+            mat['setstruct']['RVEpiX'][0][0] = rvepix
+            mat['setstruct']['RVEpiY'][0][0] = rvepiy
 
+            # print('    Dimensions EndoX: ', mat['setstruct']['EndoX'][0][0].ndim)
+            # print('    Shape EndoX: ', mat['setstruct']['EndoX'][0][0].shape)
+            # print('    Size EndoX: ', mat['setstruct']['EndoX'][0][0].size)
+
+            sio.savemat(f'{data_dir}/output/paciente_cine_2/Patient_2_Editado.mat', mat)
+            print('    Arquivo .MAT reescrito.')
+        else:
+            print('    Erro: Variável "setstruct" não encontrada no arquivo .MAT.')
+            
 print ('Done.')
--- read_mat.py
+++ read_mat.py
@@ -1,28 +1,49 @@
 import scipy.io as sio # type: ignore
-import numpy as np # type: ignore
+import numpy as np
 
-mat_contents = sio.loadmat('Patient_3.mat')
+data = sio.loadmat('Patient_3.mat')
 
-""" if 'contours_color_VE_13' in mat_contents:
-    dados = mat_contents['contours_color_VE_13']
-    print(f'Countours_color_1:\n{dados}')
-else:
-    print("Não encontrado.")
-
-if 'contours_color_VD_13' in mat_contents:
-    dados = mat_contents['contours_color_VD_13']
-    print(f'Countours_color_2:\n{dados}')
+""" if 'setstruct' in data:
+    dados = data['setstruct']
+    print(f'setstruct:\n{dados}')
 else:
     print("Não encontrado.") """
 
-print(mat_contents.keys())
+for var_name, var_value in data.items():
+    if (var_name == 'setstruct'):
+        print(f"\nVariável: {var_name}")
+        print(f"Tipo: {type(var_value)}")
+        print(f"Shape: {var_value.shape}")
+        # print(f"Dtype: {var_value.dtype}")
+        # print(var_value['EndoY'])
+    
+endoX = data['setstruct']['EndoX'][0][0] #ve
+xlv = endoX[:,0,8]
+endoY = data['setstruct']['EndoY'][0][0]
+ylv = endoY[:,0,8]
 
-# Exibir as variáveis de nível superior
+RVEndoX = data['setstruct']['RVEndoX'][0][0] #vd
+xrv = RVEndoX[:,0,8]
+RVEndoY = data['setstruct']['RVEndoY'][0][0]
+yrv = RVEndoY[:,0,8]
+
+RVEpiX = data['setstruct']['RVEpiX'][0][0] # epi
+xepi = RVEpiX[:,0,8]
+RVEpiY = data['setstruct']['RVEpiY'][0][0]
+yepi = RVEpiY[:,0,8]
+
+
+print(xlv)
+print(endoY)
+
+# print(data.keys())
+
+""" # Exibir as variáveis de nível superior
 print("## Variáveis de Nível Superior e Seus Valores ##")
-for var_name, var_value in mat_contents.items():
+for var_name, var_value in data.items():
     print(f"\nVariável: {var_name}")
     print(f"Tipo: {type(var_value)}")
     # Se for um array numpy, mostrar mais detalhes
     if isinstance(var_value, np.ndarray):
         print(f"Shape: {var_value.shape}")
-        print(f"Dtype: {var_value.dtype}")
+        print(f"Dtype: {var_value.dtype}") """
